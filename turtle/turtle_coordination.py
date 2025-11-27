1599aeb052e9880507076e3a818a049597da1c6d
dharshan-2007 <msdharshan080@gmail.com>
2025-11-27 11:40:12 +0530

    k

--- turtle/turtle_coordination.py
+++ turtle/turtle_coordination.py
@@ -1,8 +1,22 @@
 
-
+import turtle
+import random
 
 """
 turtle.textinput(title,promt)
 turtle.numinput(title,promt)
 used to show a popup and get input from the user
 """
+sc=turtle.Screen()
+sc.setup(width=500,height=400)
+user_bet=sc.textinput(title="Make your bet",prompt="Which turtle will win the race..? Enter a color : ")
+
+colors=["red","orange", "Yellow","green","blue","purple"]
+
+
+t=turtle.Turtle(shape="turtle")
+#or t.shape("turtle")
+t.penup()
+t.goto(-230)
+t.pendown()
+sc.exitonclick()
